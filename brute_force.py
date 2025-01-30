a6ba6f0f8ade5c6c676f2171f92d63aaa41c77f4
matthewthomasbeck <matthewthomasbeck@gmail.com>
2025-01-30 13:17:48 -0600

    Initial commit from PyCharm

---
+++ brute_force.py
@@ -0,0 +1,31 @@
+################################################################################
+# This code and its associated files were created at the instruction of        #
+# professors at the University of Texas at San Antonio during my time as a     #
+# student at the university. I, as a student, was not responsible for the idea #
+# behind this code (i.e. project guidelines, functionality, and end purpose),  #
+# but I, Matthew Thomas Beck, can confirm that myself and any project partners #
+# (if applicable) were the ones responsible for writing it.                    #
+################################################################################
+
+
+
+
+
+###########################################
+############### BRUTE FORCE ###############
+###########################################
+
+def two_sum():
+    nums = list(map(int, input("Enter the input data array items: ").split()))
+    target = int(input("Enter the target value: "))
+
+    # Brute force approach using two loops
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            print(f"Checking pair: ({nums[i]}, {nums[j]}) at indices ({i}, {j})")
+            if nums[i] + nums[j] == target:
+                print(f"Output: {j} {i}")
+                return
+
+# Run the function
+two_sum()
---
+++ hash_map.py
@@ -0,0 +1,35 @@
+################################################################################
+# This code and its associated files were created at the instruction of        #
+# professors at the University of Texas at San Antonio during my time as a     #
+# student at the university. I, as a student, was not responsible for the idea #
+# behind this code (i.e. project guidelines, functionality, and end purpose),  #
+# but I, Matthew Thomas Beck, can confirm that myself and any project partners #
+# (if applicable) were the ones responsible for writing it.                    #
+################################################################################
+
+
+
+
+
+########################################
+############### HASH MAP ###############
+########################################
+
+def two_sum():
+
+    nums = list(map(int, input("Enter the input data array items: ").split()))
+
+    target = int(input("Enter the target value: "))
+
+    num_map = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            print(f"Output: {i} {num_map[complement]}")
+            return
+        num_map[num] = i
+        print(f"Adding to hash table: key:{num}, value: {i}")
+
+# Run the function
+two_sum()
